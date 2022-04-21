4539af0c8a21bdfffb93ad65d8c8f487d15a99da
BabsD <aboubacar.diaw17@gmail.com>
2022-04-21 23:18:08 +0200

    complete challenge

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 
-for(let i = 0;i<5;i++) {
-    document.querySelectorAll(".numberCircle")[i].addEventListener("click",
-    function(){
-        document.querySelectorAll(".numberCircle")[i].classList.add("numberCircleSelected");
+const ratings = document.querySelectorAll(".numberCircle");
+
+ratingSelected = false;
+ratings.forEach((element) => {
+    element.addEventListener("click",() => {
+        for(let rate of ratings) {
+            if(rate.classList.contains("numberCircleSelected")) {
+                rate.classList.remove("numberCircleSelected");
+            }
+        }
+        console.log(element);
+        element.classList.add("numberCircleSelected");
+        ratingSelected = true;
     });
-}
+});
+
+
+document.querySelector("button").addEventListener("click",()=>{
+    if(!ratingSelected) {
+        alert("Please select a rating");
+        
+    }
+    else {
+        document.querySelector(".ratings").classList.add("hideRatings");
+        document.querySelector(".thanking").classList.remove("hideThanking");
+    }
+
+});
